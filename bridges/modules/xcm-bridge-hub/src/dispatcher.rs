paritytech/parity-bridges-common

Implement ExportXcm and MessageDispatch for pallet-xcm-bridge-hub (#2285)
* implement ExportXcm and MessageDispatch for pallet-xcm-bridge-hub

* spelling

modules/xcm-bridge-hub/src/dispatcher.rs
@@ -1,0 +1,103 @@
+// Copyright 2019-2021 Parity Technologies (UK) Ltd.
+// This file is part of Parity Bridges Common.
+
+// Parity Bridges Common is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Parity Bridges Common is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Parity Bridges Common.  If not, see <http://www.gnu.org/licenses/>.
+
+//! The code that allows to use the pallet (`pallet-xcm-bridge-hub`) as inbound
+//! bridge messages dispatcher. Internally, it just forwards inbound blob to the
+//! XCM-level blob dispatcher, which pushes message to some other queue (e.g.
+//! to HRMP queue with the sibling target chain).
+
+use crate::{Config, Pallet, XcmAsPlainPayload, LOG_TARGET};
+
+use bp_messages::target_chain::{DispatchMessage, MessageDispatch};
+use bp_runtime::messages::MessageDispatchResult;
+use codec::{Decode, Encode};
+use frame_support::{dispatch::Weight, CloneNoBound, EqNoBound, PartialEqNoBound};
+use pallet_bridge_messages::{Config as BridgeMessagesConfig, WeightInfoExt};
+use scale_info::TypeInfo;
+use sp_runtime::SaturatedConversion;
+use xcm_builder::{DispatchBlob, DispatchBlobError};
+
+/// Message dispatch result type for single message.
+#[derive(CloneNoBound, EqNoBound, PartialEqNoBound, Encode, Decode, Debug, TypeInfo)]
+pub enum XcmBlobMessageDispatchResult {
+	/// We've been unable to decode message payload.
+	InvalidPayload,
+	/// Message has been dispatched.
+	Dispatched,
+	/// Message has **NOT** been dispatched because of given error.
+	NotDispatched(#[codec(skip)] Option<DispatchBlobError>),
+}
+
+/// An easy way to access associated messages pallet weights.
+type MessagesPalletWeights<T, I> =
+	<T as BridgeMessagesConfig<<T as Config<I>>::BridgeMessagesPalletInstance>>::WeightInfo;
+
+impl<T: Config<I>, I: 'static> MessageDispatch for Pallet<T, I>
+where
+	T: BridgeMessagesConfig<InboundPayload = XcmAsPlainPayload>,
+{
+	type DispatchPayload = XcmAsPlainPayload;
+	type DispatchLevelResult = XcmBlobMessageDispatchResult;
+
+	fn dispatch_weight(message: &mut DispatchMessage<Self::DispatchPayload>) -> Weight {
+		match message.data.payload {
+			Ok(ref payload) => {
+				let payload_size = payload.encoded_size().saturated_into();
+				MessagesPalletWeights::<T, I>::message_dispatch_weight(payload_size)
+			},
+			Err(_) => Weight::zero(),
+		}
+	}
+
+	fn dispatch(
+		message: DispatchMessage<Self::DispatchPayload>,
+	) -> MessageDispatchResult<Self::DispatchLevelResult> {
+		let payload = match message.data.payload {
+			Ok(payload) => payload,
+			Err(e) => {
+				log::error!(
+					target: LOG_TARGET,
+					"[XcmBlobMessageDispatch] payload error: {:?} - message_nonce: {:?}",
+					e,
+					message.key.nonce
+				);
+				return MessageDispatchResult {
+					unspent_weight: Weight::zero(),
+					dispatch_level_result: XcmBlobMessageDispatchResult::InvalidPayload,
+				}
+			},
+		};
+		let dispatch_level_result = match T::BlobDispatcher::dispatch_blob(payload) {
+			Ok(_) => {
+				log::debug!(
+					target: LOG_TARGET,
+					"[XcmBlobMessageDispatch] DispatchBlob::dispatch_blob was ok - message_nonce: {:?}",
+					message.key.nonce
+				);
+				XcmBlobMessageDispatchResult::Dispatched
+			},
+			Err(e) => {
+				log::error!(
+					target: LOG_TARGET,
+					"[XcmBlobMessageDispatch] DispatchBlob::dispatch_blob failed, error: {:?} - message_nonce: {:?}",
+					e, message.key.nonce
+				);
+				XcmBlobMessageDispatchResult::NotDispatched(Some(e))
+			},
+		};
+		MessageDispatchResult { unspent_weight: Weight::zero(), dispatch_level_result }
+	}
+}

modules/xcm-bridge-hub/src/lib.rs
@@ -61,11 +61,19 @@ use frame_system::Config as SystemConfig;
 use pallet_bridge_messages::{Config as BridgeMessagesConfig, LanesManagerError};
 use sp_runtime::traits::{Header as HeaderT, HeaderProvider, Zero};
 use xcm::prelude::*;
+use xcm_builder::DispatchBlob;
 use xcm_executor::traits::ConvertLocation;
 
+pub use dispatcher::XcmBlobMessageDispatchResult;
 pub use pallet::*;
 
+mod dispatcher;
+mod exporter;
 mod mock;
+
+/// Encoded XCM blob. We expect the bridge messages pallet to use this blobtype for both inbound
+/// and outbound payloads.
+pub type XcmAsPlainPayload = Vec<u8>;
 
 /// The target that will be used when publishing logs related to this pallet.
 pub const LOG_TARGET: &str = "runtime::bridge-xcm";
@@ -115,6 +123,11 @@ pub mod pallet {
 		type BridgeReserve: Get<BalanceOf<ThisChainOf<Self, I>>>;
 		/// Currency used to pay for bridge registration.
 		type NativeCurrency: ReservableCurrency<Self::AccountId>;
+
+		/// XCM-level dispatcher for inbound bridge messages.
+		type BlobDispatcher: DispatchBlob;
+		/// Price of single message export.
+		type MessageExportPrice: Get<MultiAssets>;
 	}
 
 	/// An alias for the bridge metadata.
@@ -155,7 +168,8 @@ pub mod pallet {
 			bridge_destination_universal_location: Box<VersionedInteriorMultiLocation>,
 		) -> DispatchResult {
 			// check and compute required bridge locations
-			let locations = Self::bridge_locations(origin, bridge_destination_universal_location)?;
+			let locations =
+				Self::bridge_locations_from_origin(origin, bridge_destination_universal_location)?;
 
 			// reserve balance on the parachain sovereign account
 			let reserve = T::BridgeReserve::get();
@@ -235,7 +249,8 @@ pub mod pallet {
 			may_prune_messages: MessageNonce,
 		) -> DispatchResult {
 			// compute required bridge locations
-			let locations = Self::bridge_locations(origin, bridge_destination_universal_location)?;
+			let locations =
+				Self::bridge_locations_from_origin(origin, bridge_destination_universal_location)?;
 
 			// TODO: https://github.com/paritytech/parity-bridges-common/issues/1760 - may do refund here, if
 			// bridge/lanes are already closed + for messages that are not pruned
@@ -335,21 +350,28 @@ pub mod pallet {
 		}
 	}
 
-	impl<T: Config<I>, I: 'static> Pallet<T, I>
-	where
-		T: frame_system::Config<AccountId = AccountIdOf<ThisChainOf<T, I>>>,
-		<<T as frame_system::Config>::Block as HeaderProvider>::HeaderT:
-			HeaderT<Number = BlockNumberOf<ThisChainOf<T, I>>>,
-		T::NativeCurrency: Currency<T::AccountId, Balance = BalanceOf<ThisChainOf<T, I>>>,
-	{
+	impl<T: Config<I>, I: 'static> Pallet<T, I> {
+		/// Return bridge endpoint locations and dedicated lane identifier. This method converts
+		/// runtime `origin` argument to relative `MultiLocation` using the `T::OpenBridgeOrigin`
+		/// converter.
+		pub fn bridge_locations_from_origin(
+			origin: OriginFor<T>,
+			bridge_destination_universal_location: Box<VersionedInteriorMultiLocation>,
+		) -> Result<Box<BridgeLocations>, sp_runtime::DispatchError> {
+			Self::bridge_locations(
+				Box::new(T::OpenBridgeOrigin::ensure_origin(origin)?),
+				bridge_destination_universal_location,
+			)
+		}
+
 		/// Return bridge endpoint locations and dedicated lane identifier.
 		pub fn bridge_locations(
-			origin: OriginFor<T>,
+			bridge_origin_relative_location: Box<MultiLocation>,
 			bridge_destination_universal_location: Box<VersionedInteriorMultiLocation>,
 		) -> Result<Box<BridgeLocations>, sp_runtime::DispatchError> {
 			bridge_locations(
 				Box::new(T::UniversalLocation::get()),
-				Box::new(T::OpenBridgeOrigin::ensure_origin(origin)?),
+				bridge_origin_relative_location,
 				Box::new(
 					(*bridge_destination_universal_location)
 						.try_into()
@@ -439,7 +461,8 @@ mod tests {
 		with: InteriorMultiLocation,
 	) -> (BridgeOf<TestRuntime, ()>, BridgeLocations) {
 		let reserve = BridgeReserve::get();
-		let locations = XcmOverBridge::bridge_locations(origin, Box::new(with.into())).unwrap();
+		let locations =
+			XcmOverBridge::bridge_locations_from_origin(origin, Box::new(with.into())).unwrap();
 		let bridge_owner_account =
 			fund_origin_sovereign_account(&locations, reserve + ExistentialDeposit::get());
 		Balances::reserve(&bridge_owner_account, reserve).unwrap();
@@ -581,7 +604,7 @@ mod tests {
 	fn open_bridge_fails_if_it_already_exists() {
 		run_test(|| {
 			let origin = OpenBridgeOrigin::parent_relay_chain_origin();
-			let locations = XcmOverBridge::bridge_locations(
+			let locations = XcmOverBridge::bridge_locations_from_origin(
 				origin.clone(),
 				Box::new(bridged_asset_hub_location().into()),
 			)
@@ -614,7 +637,7 @@ mod tests {
 	fn open_bridge_fails_if_its_lanes_already_exists() {
 		run_test(|| {
 			let origin = OpenBridgeOrigin::parent_relay_chain_origin();
-			let locations = XcmOverBridge::bridge_locations(
+			let locations = XcmOverBridge::bridge_locations_from_origin(
 				origin.clone(),
 				Box::new(bridged_asset_hub_location().into()),
 			)
@@ -666,7 +689,7 @@ mod tests {
 				System::reset_events();
 
 				// compute all other locations
-				let locations = XcmOverBridge::bridge_locations(
+				let locations = XcmOverBridge::bridge_locations_from_origin(
 					origin.clone(),
 					Box::new(bridged_asset_hub_location().into()),
 				)

yarsawyer/polkadot-sdk

bridges/modules/xcm-bridge-hub/src/dispatcher.rs
@@ -1,0 +1,107 @@
+// Copyright 2019-2021 Parity Technologies (UK) Ltd.
+// This file is part of Parity Bridges Common.
+
+// Parity Bridges Common is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Parity Bridges Common is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Parity Bridges Common.  If not, see <http://www.gnu.org/licenses/>.
+
+//! The code that allows to use the pallet (`pallet-xcm-bridge-hub`) as inbound
+//! bridge messages dispatcher. Internally, it just forwards inbound blob to the
+//! XCM-level blob dispatcher, which pushes message to some other queue (e.g.
+//! to HRMP queue with the sibling target chain).
+
+use crate::{Config, Pallet, XcmAsPlainPayload, LOG_TARGET};
+
+use bp_messages::target_chain::{DispatchMessage, MessageDispatch};
+use bp_runtime::messages::MessageDispatchResult;
+use codec::{Decode, Encode};
+use frame_support::{weights::Weight, CloneNoBound, EqNoBound, PartialEqNoBound};
+use pallet_bridge_messages::{Config as BridgeMessagesConfig, WeightInfoExt};
+use scale_info::TypeInfo;
+use sp_runtime::SaturatedConversion;
+use xcm_builder::{DispatchBlob, DispatchBlobError};
+
+/// Message dispatch result type for single message.
+#[derive(CloneNoBound, EqNoBound, PartialEqNoBound, Encode, Decode, Debug, TypeInfo)]
+pub enum XcmBlobMessageDispatchResult {
+	/// We've been unable to decode message payload.
+	InvalidPayload,
+	/// Message has been dispatched.
+	Dispatched,
+	/// Message has **NOT** been dispatched because of given error.
+	NotDispatched(#[codec(skip)] Option<DispatchBlobError>),
+}
+
+/// An easy way to access associated messages pallet weights.
+type MessagesPalletWeights<T, I> =
+	<T as BridgeMessagesConfig<<T as Config<I>>::BridgeMessagesPalletInstance>>::WeightInfo;
+
+impl<T: Config<I>, I: 'static> MessageDispatch for Pallet<T, I>
+where
+	T: BridgeMessagesConfig<InboundPayload = XcmAsPlainPayload>,
+{
+	type DispatchPayload = XcmAsPlainPayload;
+	type DispatchLevelResult = XcmBlobMessageDispatchResult;
+
+	fn is_active() -> bool {
+		true
+	}
+
+	fn dispatch_weight(message: &mut DispatchMessage<Self::DispatchPayload>) -> Weight {
+		match message.data.payload {
+			Ok(ref payload) => {
+				let payload_size = payload.encoded_size().saturated_into();
+				MessagesPalletWeights::<T, I>::message_dispatch_weight(payload_size)
+			},
+			Err(_) => Weight::zero(),
+		}
+	}
+
+	fn dispatch(
+		message: DispatchMessage<Self::DispatchPayload>,
+	) -> MessageDispatchResult<Self::DispatchLevelResult> {
+		let payload = match message.data.payload {
+			Ok(payload) => payload,
+			Err(e) => {
+				log::error!(
+					target: LOG_TARGET,
+					"[XcmBlobMessageDispatch] payload error: {:?} - message_nonce: {:?}",
+					e,
+					message.key.nonce
+				);
+				return MessageDispatchResult {
+					unspent_weight: Weight::zero(),
+					dispatch_level_result: XcmBlobMessageDispatchResult::InvalidPayload,
+				}
+			},
+		};
+		let dispatch_level_result = match T::BlobDispatcher::dispatch_blob(payload) {
+			Ok(_) => {
+				log::debug!(
+					target: LOG_TARGET,
+					"[XcmBlobMessageDispatch] DispatchBlob::dispatch_blob was ok - message_nonce: {:?}",
+					message.key.nonce
+				);
+				XcmBlobMessageDispatchResult::Dispatched
+			},
+			Err(e) => {
+				log::error!(
+					target: LOG_TARGET,
+					"[XcmBlobMessageDispatch] DispatchBlob::dispatch_blob failed, error: {:?} - message_nonce: {:?}",
+					e, message.key.nonce
+				);
+				XcmBlobMessageDispatchResult::NotDispatched(Some(e))
+			},
+		};
+		MessageDispatchResult { unspent_weight: Weight::zero(), dispatch_level_result }
+	}
+}

bridges/modules/xcm-bridge-hub/src/lib.rs
@@ -51,24 +51,24 @@
 #![warn(missing_docs)]
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use bridge_runtime_common::messages_xcm_extension::XcmBlobHauler;
-
-pub use exporter::PalletAsHaulBlobExporter;
-pub use pallet::*;
-
-mod exporter;
 use bp_messages::{LaneId, LaneState, MessageNonce};
 use bp_runtime::{AccountIdOf, BalanceOf, RangeInclusiveExt};
 use bp_xcm_bridge_hub::{
-	bridge_locations, Bridge, BridgeLocations, BridgeLocationsError, BridgeState,
+	bridge_locations, Bridge, BridgeLocations, BridgeLocationsError, BridgeState, XcmAsPlainPayload,
 };
 use frame_support::traits::{Currency, ReservableCurrency};
 use frame_system::Config as SystemConfig;
 use pallet_bridge_messages::{Config as BridgeMessagesConfig, LanesManagerError};
 use sp_runtime::traits::Zero;
 use xcm::prelude::*;
+use xcm_builder::DispatchBlob;
 use xcm_executor::traits::ConvertLocation;
 
+pub use dispatcher::XcmBlobMessageDispatchResult;
+pub use pallet::*;
+
+mod dispatcher;
+mod exporter;
 mod mock;
 
 /// The target that will be used when publishing logs related to this pallet.
@@ -77,7 +77,6 @@ pub const LOG_TARGET: &str = "runtime::bridge-xcm";
 #[frame_support::pallet]
 pub mod pallet {
 	use super::*;
-	use bridge_runtime_common::messages_xcm_extension::SenderAndLane;
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::{BlockNumberFor, *};
 
@@ -107,13 +106,6 @@ pub mod pallet {
 		/// Checks the XCM version for the destination.
 		type DestinationVersion: GetVersion;
 
-		/// Get point-to-point links with bridged consensus (`Self::BridgedNetworkId`).
-		/// (this will be replaced with dynamic on-chain bridges - `Bridges V2`)
-		type Lanes: Get<sp_std::vec::Vec<(SenderAndLane, (NetworkId, InteriorLocation))>>;
-		/// Support for point-to-point links
-		/// (this will be replaced with dynamic on-chain bridges - `Bridges V2`)
-		type LanesSupport: XcmBlobHauler;
-
 		/// A set of XCM locations within local consensus system that are allowed to open
 		/// bridges with remote destinations.
 		// TODO: there's only one impl of `EnsureOrigin<Success = Location>` -
@@ -132,6 +124,9 @@ pub mod pallet {
 		type BridgeReserve: Get<BalanceOf<ThisChainOf<Self, I>>>;
 		/// Currency used to pay for bridge registration.
 		type NativeCurrency: ReservableCurrency<Self::AccountId>;
+
+		/// XCM-level dispatcher for inbound bridge messages.
+		type BlobDispatcher: DispatchBlob;
 	}
 
 	/// An alias for the bridge metadata.
@@ -182,7 +177,7 @@ pub mod pallet {
 		) -> DispatchResult {
 			// check and compute required bridge locations
 			let locations =
-				Self::bridge_locations(origin, bridge_destination_universal_location.clone())?;
+				Self::bridge_locations_from_origin(origin, bridge_destination_universal_location)?;
 
 			// reserve balance on the parachain sovereign account
 			let reserve = T::BridgeReserve::get();
@@ -262,7 +257,8 @@ pub mod pallet {
 			may_prune_messages: MessageNonce,
 		) -> DispatchResult {
 			// compute required bridge locations
-			let locations = Self::bridge_locations(origin, bridge_destination_universal_location)?;
+			let locations =
+				Self::bridge_locations_from_origin(origin, bridge_destination_universal_location)?;
 
 			// TODO: https://github.com/paritytech/parity-bridges-common/issues/1760 - may do refund here, if
 			// bridge/lanes are already closed + for messages that are not pruned
@@ -362,19 +358,28 @@ pub mod pallet {
 		}
 	}
 
-	impl<T: Config<I>, I: 'static> Pallet<T, I>
-	where
-		T: frame_system::Config<AccountId = AccountIdOf<ThisChainOf<T, I>>>,
-		T::NativeCurrency: Currency<T::AccountId, Balance = BalanceOf<ThisChainOf<T, I>>>,
-	{
+	impl<T: Config<I>, I: 'static> Pallet<T, I> {
+		/// Return bridge endpoint locations and dedicated lane identifier. This method converts
+		/// runtime `origin` argument to relative `Location` using the `T::OpenBridgeOrigin`
+		/// converter.
+		pub fn bridge_locations_from_origin(
+			origin: OriginFor<T>,
+			bridge_destination_universal_location: Box<VersionedInteriorLocation>,
+		) -> Result<Box<BridgeLocations>, sp_runtime::DispatchError> {
+			Self::bridge_locations(
+				Box::new(T::OpenBridgeOrigin::ensure_origin(origin)?),
+				bridge_destination_universal_location,
+			)
+		}
+
 		/// Return bridge endpoint locations and dedicated lane identifier.
 		pub fn bridge_locations(
-			origin: OriginFor<T>,
+			bridge_origin_relative_location: Box<Location>,
 			bridge_destination_universal_location: Box<VersionedInteriorLocation>,
 		) -> Result<Box<BridgeLocations>, sp_runtime::DispatchError> {
 			bridge_locations(
 				Box::new(T::UniversalLocation::get()),
-				Box::new(T::OpenBridgeOrigin::ensure_origin(origin)?),
+				bridge_origin_relative_location,
 				Box::new(
 					(*bridge_destination_universal_location)
 						.try_into()
@@ -454,31 +459,6 @@ pub mod pallet {
 		/// The version of XCM location argument is unsupported.
 		UnsupportedXcmVersion,
 	}
-
-	impl<T: Config<I>, I: 'static> Pallet<T, I> {
-		/// Returns dedicated/configured lane identifier.
-		pub(crate) fn lane_for(
-			source: &InteriorLocation,
-			dest: (&NetworkId, &InteriorLocation),
-		) -> Option<SenderAndLane> {
-			let source = source.clone().relative_to(&T::UniversalLocation::get());
-
-			// Check that we have configured a point-to-point lane for 'source' and `dest`.
-			T::Lanes::get()
-				.into_iter()
-				.find_map(|(lane_source, (lane_dest_network, lane_dest))| {
-					if lane_source.location == source &&
-						&lane_dest_network == dest.0 &&
-						Self::bridged_network_id().as_ref() == Ok(dest.0) &&
-						&lane_dest == dest.1
-					{
-						Some(lane_source)
-					} else {
-						None
-					}
-				})
-		}
-	}
 }
 
 #[cfg(test)]
@@ -502,7 +482,8 @@ mod tests {
 		with: InteriorLocation,
 	) -> (BridgeOf<TestRuntime, ()>, BridgeLocations) {
 		let reserve = BridgeReserve::get();
-		let locations = XcmOverBridge::bridge_locations(origin, Box::new(with.into())).unwrap();
+		let locations =
+			XcmOverBridge::bridge_locations_from_origin(origin, Box::new(with.into())).unwrap();
 		let bridge_owner_account =
 			fund_origin_sovereign_account(&locations, reserve + ExistentialDeposit::get());
 		Balances::reserve(&bridge_owner_account, reserve).unwrap();
@@ -643,7 +624,7 @@ mod tests {
 	fn open_bridge_fails_if_it_already_exists() {
 		run_test(|| {
 			let origin = OpenBridgeOrigin::parent_relay_chain_origin();
-			let locations = XcmOverBridge::bridge_locations(
+			let locations = XcmOverBridge::bridge_locations_from_origin(
 				origin.clone(),
 				Box::new(bridged_asset_hub_location().into()),
 			)
@@ -676,7 +657,7 @@ mod tests {
 	fn open_bridge_fails_if_its_lanes_already_exists() {
 		run_test(|| {
 			let origin = OpenBridgeOrigin::parent_relay_chain_origin();
-			let locations = XcmOverBridge::bridge_locations(
+			let locations = XcmOverBridge::bridge_locations_from_origin(
 				origin.clone(),
 				Box::new(bridged_asset_hub_location().into()),
 			)
@@ -728,7 +709,7 @@ mod tests {
 				System::reset_events();
 
 				// compute all other locations
-				let locations = XcmOverBridge::bridge_locations(
+				let locations = XcmOverBridge::bridge_locations_from_origin(
 					origin.clone(),
 					Box::new(bridged_asset_hub_location().into()),
 				)